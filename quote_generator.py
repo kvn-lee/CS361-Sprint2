6000d1150e2a2986368b4c82d5a2b86158054ecd
Kevin Lee <kevin.son.lee@gmail.com>
2024-05-15 11:29:55 -0400

    implemented a quote generator

---
+++ quote_generator.py
@@ -0,0 +1,35 @@
+import zmq
+import csv
+import random
+
+# Create context
+context = zmq.Context()
+
+# Create socket
+socket = context.socket(zmq.REP)
+
+# Bind socket to main program
+socket.bind("tcp://*:5000")
+
+
+def GenerateQuote():
+    """
+    Function will generate a motivational quote
+    .csv file exists with a database of quotes to use
+    """
+
+    filename = "quotes.csv"
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        rows = list(csvreader)
+
+    return rows[random.randint(1, len(rows)-1)][1]
+
+
+while True:
+    request = socket.recv()
+    print("Received request: %s" % request)
+
+    quote = GenerateQuote()
+
+    socket.send(b"Here is your quote: " + quote)
